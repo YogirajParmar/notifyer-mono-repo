fec724018a0e243f3fff49af2ed9a84579875603
Yogirajsinh Parmar <yogirajsinh.parmar@seaflux.tech>
2024-11-17 16:03:35 +0530

    fix: delete unnecessary function

--- src/frontend/scripts/renderer.ts
+++ src/frontend/scripts/renderer.ts
@@ -361,14 +361,3 @@ function updateExpiringDocuments(documents: VDocument[]) {
     });
   }
 }
-
-function formatDate(dateString: string) {
-  const date = new Date(dateString);
-  if (!date) {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  }
-  return '';
-}
