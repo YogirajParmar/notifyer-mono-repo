1d8b1689ffdeacb773d25115deb1613569f613f0
YogirajParmar <138298164+YogirajParmar@users.noreply.github.com>
2025-11-15 18:36:49 +0530

    chore: add migration logs

--- src/backend/db/umzug.ts
+++ src/backend/db/umzug.ts
@@ -1,12 +1,19 @@
 import { SequelizeStorage, Umzug } from 'umzug';
 import { getSequelize } from '../configs';
 import { Sequelize } from 'sequelize';
+import { app } from 'electron';
+import { Logger } from '../helpers/logger.helper';
 
 export class Migrator extends Umzug<Sequelize> {
+  private logger = Logger.getInstance();
+
   constructor(private sequelize: Sequelize = getSequelize()) {
     super({
       migrations: {
-        glob: ['migrations/*.ts', { cwd: __dirname }],
+        glob: [
+          app && app.isPackaged ? 'migrations/*.js' : 'migrations/*.ts',
+          { cwd: __dirname },
+        ],
       },
       context: sequelize,
       storage: new SequelizeStorage({
@@ -17,7 +24,21 @@ export class Migrator extends Umzug<Sequelize> {
   }
 
   public async runMigrations() {
-    await this.up();
+    this.logger.log(
+      'info',
+      `Applying migrations from ${app && app.isPackaged ? 'migrations/*.js' : 'migrations/*.ts'}`
+    );
+    const migrations = await this.up();
+
+    this.logger.log('info', `Applied ${migrations.length} migrations.`);
+
+    for (const migration of migrations) {
+      this.logger.log('info', `Applied migration: ${migration.name}`);
+      this.logger.log(
+        'info',
+        `Migration Path: ${JSON.stringify(migration.path)}`
+      );
+    }
   }
 }
 
